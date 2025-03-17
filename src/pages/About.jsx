90b60d489a3cbdfdcac3ba005b37a31b323e30c5
Thays Ribeiro <thaysmoiaribeiro@gmail.com>
2025-03-17 14:21:24 -0700

    Add section "About" with detailed description and special style

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import './styles/about.css';
 
 const About = () => {
     return ( 
-    <div>
+    <section className='about-section'>
         <h2>About Me</h2>
-        <p>This is the about page where you can learn more about my background and skills.</p>
-    </div>
+        <p>I am a Software Engineer passionate about creating scalable and high-performance web applications. 
+        With a strong foundation in JavaScript (ES6+), TypeScript, React.js, and Node.js, I specialize in developing
+        interactive front-end experiences and robust back-end solutions. I also have experience working with relational
+        and non-relational databases, microservices, Docker, and CI/CD pipelines. My career journey has been diverse, 
+        giving me a unique perspective and adaptability. I have worked as an au pair, a lawyer, a logistics coordinator,
+        a facilities operator, a cleaner, a banking sector, a sales, an administration, and even a customer service. 
+        These roles have strengthened my problem-solving skills, resilience, and ability to work in fast-paced environments,
+        all of which I now apply in software development. Currently, I am honing my technical expertise through the UCI Bootcamp,
+        where I work on projects involving RESTful APIs with Express.js and PostgreSQL, front-end applications using React and Vite,
+        and best coding practices for building efficient, maintainable systems. I am now seeking opportunities where I can
+        leverage my technical skills and diverse professional background to contribute to impactful projects in the tech industry.</p>
+    </section>
     );
 }
  
