289777077204795e6b0e8db5d65bc8577ca06c74
Thays Ribeiro Maher <thaysmoiaribeiro@gmail.com>
2025-07-11 03:36:09 +0000

    Refactor the style of links in Portfolio.css to block display and change the color to inherit from the parent element

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -21,249 +21,201 @@ const Portfolio = () => {
       <p>Check out some of the projects I've developed below!</p>
 
       <div className="projects">
-        <div className="project-card">
-          <img src={analogcrono} alt="Analog Crono" className="project-image" />
-          <div className="project-info">
-            <h3>Analog Crono</h3>
-            <p>ThayRibeiro0</p>
-            <a
-              href="https://github.com/ThayRibeiro0/analogcrono"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={github}
-                alt="GitHub"
-                className="github-icon"
-              />
-            </a>
+        <a
+          href="https://github.com/ThayRibeiro0/analogcrono"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="project-card">
+            <img
+              src={analogcrono}
+              alt="Analog Crono"
+              className="project-image"
+            />
+            <div className="project-info">
+              <h3>Analog Crono</h3>
+              <p>ThayRibeiro0</p>
+              <img src={github} alt="GitHub" className="github-icon" />
+            </div>
           </div>
-        </div>
+        </a>
 
-        <div className="project-card">
-          <img src={countdown} alt="Countdown" className="project-image" />
-          <div className="project-info">
-            <h3>Countdown</h3>
-            <p>ThayRibeiro0</p>
-            <a
-              href="https://github.com/ThayRibeiro0/countdown1"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={github}
-                alt="GitHub"
-                className="github-icon"
-              />
-            </a>
+        <a
+          href="https://github.com/ThayRibeiro0/countdown1"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="project-card">
+            <img src={countdown} alt="Countdown" className="project-image" />
+            <div className="project-info">
+              <h3>Countdown</h3>
+              <p>ThayRibeiro0</p>
+              <img src={github} alt="GitHub" className="github-icon" />
+            </div>
           </div>
-        </div>
+        </a>
 
-        <div className="project-card">
-          <img
-            src={employeeTracker}
-            alt="Employee Tracker"
-            className="project-image"
-          />
-          <div className="project-info">
-            <h3>Employee Tracker</h3>
-            <p>ThayRibeiro0</p>
-            <a
-              href="https://github.com/ThayRibeiro0/employee_tracker"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={github}
-                alt="GitHub"
-                className="github-icon"
-              />
-            </a>
+        <a
+          href="https://github.com/ThayRibeiro0/employee_tracker"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="project-card">
+            <img
+              src={employeeTracker}
+              alt="Employee Tracker"
+              className="project-image"
+            />
+            <div className="project-info">
+              <h3>Employee Tracker</h3>
+              <p>ThayRibeiro0</p>
+              <img src={github} alt="GitHub" className="github-icon" />
+            </div>
           </div>
-        </div>
+        </a>
 
-        <div className="project-card">
-          <img src={myWebsite} alt="My Website" className="project-image" />
-          <div className="project-info">
-            <h3>My Website</h3>
-            <p>ThayRibeiro0</p>
-            <a
-              href="https://github.com/ThayRibeiro0/my-new-website"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={github}
-                alt="GitHub"
-                className="github-icon"
-              />
-            </a>
+        <a
+          href="https://github.com/ThayRibeiro0/my-new-website"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="project-card">
+            <img src={myWebsite} alt="My Website" className="project-image" />
+            <div className="project-info">
+              <h3>My Website</h3>
+              <p>ThayRibeiro0</p>
+              <img src={github} alt="GitHub" className="github-icon" />
+            </div>
           </div>
-        </div>
+        </a>
 
-        <div className="project-card">
-          <img
-            src={nodeReadme}
-            alt="Node README Generator"
-            className="project-image"
-          />
-          <div className="project-info">
-            <h3>Node README Generator</h3>
-            <p>ThayRibeiro0</p>
-            <a
-              href="https://github.com/ThayRibeiro0/node-readme-generator"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={github}
-                alt="GitHub"
-                className="github-icon"              
-              />
-            </a>
+        <a
+          href="https://github.com/ThayRibeiro0/node-readme-generator"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="project-card">
+            <img
+              src={nodeReadme}
+              alt="Node README Generator"
+              className="project-image"
+            />
+            <div className="project-info">
+              <h3>Node README Generator</h3>
+              <p>ThayRibeiro0</p>
+              <img src={github} alt="GitHub" className="github-icon" />
+            </div>
           </div>
-        </div>
+        </a>
 
-        <div className="project-card">
-          <img src={pokemonApp} alt="Pokemon App" className="project-image" />
-          <div className="project-info">
-            <h3>Pokemon App</h3>
-            <p>ThayRibeiro0</p>
-            <a
-              href="https://github.com/ThayRibeiro0/pokemon_app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={github}
-                alt="GitHub"
-                className="github-icon"
-              />
-            </a>
+        <a
+          href="https://github.com/ThayRibeiro0/pokemon_app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="project-card">
+            <img src={pokemonApp} alt="Pokemon App" className="project-image" />
+            <div className="project-info">
+              <h3>Pokemon App</h3>
+              <p>ThayRibeiro0</p>
+              <img src={github} alt="GitHub" className="github-icon" />
+            </div>
           </div>
-        </div>
+        </a>
 
-        <div className="project-card">
-          <img src={pyGym} alt="PyGym" className="project-image" />
-          <div className="project-info">
-            <h3>PyGym</h3>
-            <p>ThayRibeiro0</p>
-            <a
-              href="https://github.com/ThayRibeiro0/pyGym"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={github}
-                alt="GitHub"
-                className="github-icon"
-              />
-            </a>
+        <a
+          href="https://github.com/ThayRibeiro0/pyGym"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="project-card">
+            <img src={pyGym} alt="PyGym" className="project-image" />
+            <div className="project-info">
+              <h3>PyGym</h3>
+              <p>ThayRibeiro0</p>
+              <img src={github} alt="GitHub" className="github-icon" />
+            </div>
           </div>
-        </div>
+        </a>
 
-        <div className="project-card">
-          <img src={quizReact} alt="Quiz React" className="project-image" />
-          <div className="project-info">
-            <h3>Quiz React</h3>
-            <p>ThayRibeiro0</p>
-            <a
-              href="https://github.com/ThayRibeiro0/quiz-react"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={github}
-                alt="GitHub"
-                className="github-icon"
-              />
-            </a>
+        <a
+          href="https://github.com/ThayRibeiro0/quiz-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="project-card">
+            <img src={quizReact} alt="Quiz React" className="project-image" />
+            <div className="project-info">
+              <h3>Quiz React</h3>
+              <p>ThayRibeiro0</p>
+              <img src={github} alt="GitHub" className="github-icon" />
+            </div>
           </div>
-        </div>
+        </a>
 
-        <div className="project-card">
-          <img src={stargazers} alt="Stargazers" className="project-image" />
-          <div className="project-info">
-            <h3>Stargazers</h3>
-            <p>ThayRibeiro0</p>
-            <a
-              href="https://github.com/ThayRibeiro0/stargazers"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={github}
-                alt="GitHub"
-                className="github-icon"
-              />
-            </a>
+        <a
+          href="https://github.com/ThayRibeiro0/stargazers"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="project-card">
+            <img src={stargazers} alt="Stargazers" className="project-image" />
+            <div className="project-info">
+              <h3>Stargazers</h3>
+              <p>ThayRibeiro0</p>
+              <img src={github} alt="GitHub" className="github-icon" />
+            </div>
           </div>
-        </div>
+        </a>
 
-        <div className="project-card">
-          <img
-            src={vehicleConsoleApp}
-            alt="Vehicle Console App"
-            className="project-image"
-          />
-          <div className="project-info">
-            <h3>Vehicle Console App</h3>
-            <p>ThayRibeiro0</p>
-            <a
-              href="https://github.com/ThayRibeiro0/vehicle-console-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={github}
-                alt="GitHub"
-                className="github-icon"
-              />
-            </a>
+        <a
+          href="https://github.com/ThayRibeiro0/vehicle-console-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="project-card">
+            <img
+              src={vehicleConsoleApp}
+              alt="Vehicle Console App"
+              className="project-image"
+            />
+            <div className="project-info">
+              <h3>Vehicle Console App</h3>
+              <p>ThayRibeiro0</p>
+              <img src={github} alt="GitHub" className="github-icon" />
+            </div>
           </div>
-        </div>
+        </a>
 
-        <div className="project-card">
-          <img src={Costs} alt="Costs" className="project-image" />
-          <div className="project-info">
-            <h3>Costs</h3>
-            <p>ThayRibeiro0</p>
-            <a
-              href="https://github.com/ThayRibeiro0/costs"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={github}
-                alt="GitHub"
-                className="github-icon"            
-              />
-            </a>
+        <a
+          href="https://github.com/ThayRibeiro0/costs"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="project-card">
+            <img src={Costs} alt="Costs" className="project-image" />
+            <div className="project-info">
+              <h3>Costs</h3>
+              <p>ThayRibeiro0</p>
+              <img src={github} alt="GitHub" className="github-icon" />
+            </div>
           </div>
-        </div>
+        </a>
 
-        <div className="project-card">
-          <img
-            src={calculator}
-            alt="Calculator"
-            className="project-image"
-          />
-          <div className="project-info">
-            <h3>Calculator</h3>
-            <p>ThayRibeiro0</p>
-            <a
-              href="https://github.com/ThayRibeiro0/calculator"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src={github}
-                alt="GitHub"
-                className="github-icon"
-              />
-            </a>
+        <a
+          href="https://github.com/ThayRibeiro0/calculator"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="project-card">
+            <img src={calculator} alt="Calculator" className="project-image" />
+            <div className="project-info">
+              <h3>Calculator</h3>
+              <p>ThayRibeiro0</p>
+              <img src={github} alt="GitHub" className="github-icon" />
+            </div>
           </div>
-        </div>
+        </a>
       </div>
     </section>
   );
